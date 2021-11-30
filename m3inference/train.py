e041efa386f7198d8c3d2ef9460dc5b745f830dd
Sadegh Ranjbar <s.ranjbar@dataak.com>
2021-11-30 13:44:03 +0330

    Feature: #DATA-308 trainer class has been added to project

---
+++ m3inference/train.py
@@ -0,0 +1,69 @@
+from m3inference.utils import logging
+import argparse
+from m3inference.dataset import M3InferenceDataset
+from torch.utils.data import DataLoader
+from m3inference.full_model import M3InferenceModel 
+import json 
+import torch 
+
+class M3Trainer:
+
+    def __init__(self,train_data_path,batch_size=16,label='gender',train_split=0.8,validation=False):
+
+        self.train_data_path = train_data_path
+        self.data = self.read_json_file(train_data_path)
+        self.batch_size = batch_size
+        self.label_level = label
+        self.train_data , self.test_data = self.train_val_test_split(self.data,train_split)
+
+        self.train_data_loader = DataLoader(M3InferenceDataset(self.train_data,label_level = label),batch_size=self.batch_size)
+        
+        self.test_data_loader = DataLoader(M3InferenceDataset(self.test_data,label_level = label),batch_size=self.batch_size)
+
+        self.m3Model = M3InferenceModel()
+
+        self.loss_function = torch.nn.CrossEntropyLoss()
+
+        self.optimizer = torch.optim.Adam(self..m3Model.parameters())
+
+        self.logger = logging.getLogger(__name__)
+
+    def read_json_file(self,json_path):
+        with open(json_path,'r') as f:
+            file_content = json.loads(f.read())
+        return file_content
+
+
+    def train_val_test_split(self,data,train_split,validation=False):
+        len_data = len(data)
+        idx_train = int(len_data * train_split)
+        len_val_test = (len_data - idx_train) / 2
+        if validation:
+            return data[:idx_train] , data[idx_train:idx_trian+len_val_test] , data[idx_train+len_val_test:]
+        else:
+            return data[:idx_train] , data[idx_train:]
+        
+    
+    def train(self,epochs=10):
+        for epoch in range(epochs):
+
+            running_loss = 0.0
+
+            for i,batch in enumerate(self.train_data_loader,0):
+
+                x,y = batch 
+                y = y.double()
+
+                self.optimizer.zero_grad()
+
+                outputs = self.m3Model(x,label=self.label_level)
+
+                loss = self.loss_function(outputs,y)
+                loss.backward()
+                self.optimizer.spet()
+
+                running_loss += loss.item()
+
+        train_log = 'Epoch: {} loss: {}'.format(epoch+1, running_loss/i )
+        print(train_log)
+        self.logger.info(train_log)
